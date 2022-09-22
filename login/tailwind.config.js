3a9216e700214b9c1adec2d873f391a8c615495e
HadziqM <dimascrazz@gmail.com>
2022-09-22 15:10:41 +0700

    img

--- login/tailwind.config.js
+++ login/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./templates/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        logo: "/img/its.png",
+        kapal: "/img/logo.png",
+        logo1: "/img/logo1.png",
+        ship: "/img/ship.png",
+      },
+    },
   },
   plugins: [],
 };
